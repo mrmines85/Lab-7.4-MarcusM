c8365e368e7d952cc4d04c4a3c956510b2b8fbb9
mrmines85 <61034810+mrmines85@users.noreply.github.com>
2020-03-05 19:19:06 -0600

    Add files via upload
    Lab 7.4 MarcusM

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7_4DatingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatingProfile kyle = new DatingProfile("Kyle", "Reese", 30, "Male");
+            kyle.WriteBio("Soldier from the future");
+
+            DatingProfile sarah = new DatingProfile("Sarah", "Connor", 25, "Female");
+            sarah.WriteBio("It's supposed to be the 80s right? What's a dating app?");
+
+            kyle.SendMessage("Hello Sarah", "Come with me if you want to live!", sarah);
+            sarah.ReadMessage();
+        }
+    }
+}
